13cd9214f9d6c3fbc8ff9dfcb517fa2256feb614
Bruna Santos <brunasantos.ctt@gmail.com>
2025-06-14 00:06:29 -0300

    Testando Servlet

---
+++ Biblioteca/src/main/java/hello/Test.java
@@ -0,0 +1,42 @@
+package hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>teste</title>");
+		out.println("<body>");
+		out.println("<h1>Hello World</h1>");
+		out.println("oi");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
